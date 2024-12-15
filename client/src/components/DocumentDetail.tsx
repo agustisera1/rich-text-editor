a892e2057094f06372cff2b46f90fd2871119f25
agustisera1 <agustisera1@gmail.com>
2024-12-15 17:41:23 -0300

    feat(editor): scaffold base component

--- client/src/components/DocumentDetail.tsx
+++ client/src/components/DocumentDetail.tsx
@@ -1,10 +1,63 @@
-import { useContext } from "react";
-import { useParams } from "react-router";
-import { EditorContext } from "../providers";
+import StarterKit from "@tiptap/starter-kit";
+import { EditorProvider, useCurrentEditor, useEditor } from "@tiptap/react";
+import { BoldIcon, ItalicIcon } from "lucide-react";
+
+const extensions = [StarterKit];
+
+const MenuBar = () => {
+  const participants = ["Alice", "Bob", "Charlie"];
+  const { editor } = useCurrentEditor();
+  if (!editor) return null;
+
+  /* More features implementation in the same way, bulleted list, strike, etc. */
+  const toggleItalic = () => {
+    editor.chain().focus().toggleItalic().run();
+  };
+
+  const toggleBold = () => {
+    editor.chain().focus().toggleBold().run();
+  };
+
+  return (
+    <div className="editor-buttons-container">
+      <div>
+        <button
+          className={`editor-button ${
+            editor.isActive("italic") && "active-btn"
+          }`}
+          onClick={toggleItalic}
+        >
+          <ItalicIcon size={15} />
+        </button>
+        <button
+          className={`editor-button ${editor.isActive("bold") && "active-btn"}`}
+          onClick={toggleBold}
+        >
+          <BoldIcon size={15} />
+        </button>
+      </div>
+      <div className="participants-container">
+        {participants.map((participant) => (
+          <div className="participant-avatar" key={participant}>
+            {participant[0].toUpperCase()}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export const DocumentDetail = () => {
-  const { id } = useParams();
-  const { documents } = useContext(EditorContext);
-  const document = documents.find((doc) => doc.id === id);
-  return <div style={{ textAlign: "center" }}>{JSON.stringify(document)}</div>;
+  // const { id } = useParams();
+  // const { documents } = useContext(EditorContext);
+  return (
+    <div className="editor-wrapper">
+      <EditorProvider
+        content={"Document content here"}
+        extensions={extensions}
+        autofocus={true}
+        slotBefore={<MenuBar />}
+      ></EditorProvider>
+    </div>
+  );
 };
